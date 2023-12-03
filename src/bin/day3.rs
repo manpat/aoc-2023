b2c9c5731a8a966ce459586e5f1029a763aff455
Patrick Monaghan <0x5f.manpat@gmail.com>
2023-12-03 16:19:26 +0000

    day3 part 2

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -6,12 +6,19 @@ fn main() {
 
 	let map = parse_map(include_str!("day3input.txt"));
 
-	let part_1: u32 = map.numbers.iter()
-		.filter(|n| n.is_part)
+	let part_1: u32 = map.part_numbers.iter()
 		.map(|n| n.value)
 		.sum();
 
 	dbg!(part_1);
+
+	let part_2: u32 = map.symbols.iter()
+		.filter(|sym| sym.ch == '*')
+		.filter_map(|sym| get_exactly_two_adjacent_values(sym.pos, &map.part_numbers))
+		.map(|(a, b)| a * b) // gear ratio
+		.sum();
+
+	dbg!(part_2);
 }
 
 
@@ -49,12 +56,15 @@ fn parse_map(document: &str) -> Map {
 
 			let span = number_str.len();
 
-			map.numbers.push(Number {
+			let number = Number {
 				pos,
 				value: number_str.parse().unwrap(),
 				span: span as i32,
-				is_part: span_has_adjacent_symbol(pos, span as i32, &map.symbols),
-			});
+			};
+
+			if number_has_adjacent_symbol(&number, &map.symbols) {
+				map.part_numbers.push(number);
+			}
 
 			search_start += found_index + span;
 		}
@@ -64,25 +74,36 @@ fn parse_map(document: &str) -> Map {
 }
 
 
-fn span_has_adjacent_symbol(pos: Vec2i, span: i32, symbols: &[Symbol]) -> bool {
-	let min_bounds = pos - Vec2i::splat(1);
-	let max_bounds = pos + Vec2i::new(span, 1);
+fn get_exactly_two_adjacent_values(pos: Vec2i, numbers: &[Number]) -> Option<(u32, u32)> {
+	let numbers = {
+		let start_index = numbers.partition_point(|num| num.pos.y < pos.y - 1);
+		let end_index = numbers[start_index..].partition_point(|num| num.pos.y <= pos.y + 1);
+		&numbers[start_index..start_index + end_index]
+	};
+
+	let mut it = numbers.iter().filter(|num| num.bounds().contains(pos)).map(|num| num.value);
+	let result = (it.next()?, it.next()?);
+
+	if it.next().is_none() {
+		Some(result)
+	} else {
+		None
+	}
+}
+
+fn number_has_adjacent_symbol(number: &Number, symbols: &[Symbol]) -> bool {
+	let bounds = number.bounds();
 
 	let symbol_scope = {
-		let start_index = symbols.partition_point(|sym| cmp_vec2i(sym.pos, min_bounds).is_lt());
-		let end_index = symbols[start_index..].partition_point(|sym| cmp_vec2i(sym.pos, max_bounds).is_le());
+		let start_index = symbols.partition_point(|sym| cmp_vec2i(sym.pos, bounds.min).is_lt());
+		let end_index = symbols[start_index..].partition_point(|sym| cmp_vec2i(sym.pos, bounds.max).is_le());
 		&symbols[start_index..start_index + end_index]
 	};
 
 	symbol_scope.iter()
-		.any(move |sym| is_point_in_bounds(sym.pos, min_bounds, max_bounds))
+		.any(move |sym| bounds.contains(sym.pos))
 }
 
-fn is_point_in_bounds(pos: Vec2i, min_bounds: Vec2i, max_bounds: Vec2i) -> bool {
-	// both bounds are inclusive
-	(min_bounds.x..=max_bounds.x).contains(&pos.x)
-	&& (min_bounds.y..=max_bounds.y).contains(&pos.y)
-}
 
 
 use std::cmp::Ordering;
@@ -91,12 +112,24 @@ fn cmp_vec2i(a: Vec2i, b: Vec2i) -> Ordering {
 	a.y.cmp(&b.y).then(a.x.cmp(&b.x))
 }
 
+struct Bounds {
+	min: Vec2i,
+	max: Vec2i,
+}
+
+impl Bounds {
+	fn contains(&self, pos: Vec2i) -> bool {
+		// both bounds are inclusive
+		(self.min.x..=self.max.x).contains(&pos.x)
+		&& (self.min.y..=self.max.y).contains(&pos.y)
+	}
+}
 
 
 #[derive(Debug, Default)]
 struct Map {
 	symbols: Vec<Symbol>,
-	numbers: Vec<Number>,
+	part_numbers: Vec<Number>,
 }
 
 #[derive(Debug)]
@@ -105,7 +138,15 @@ struct Number {
 	span: i32,
 
 	value: u32,
-	is_part: bool,
+}
+
+impl Number {
+	fn bounds(&self) -> Bounds {
+		Bounds {
+			min: self.pos - Vec2i::splat(1),
+			max: self.pos + Vec2i::new(self.span, 1),
+		}
+	}
 }
 
 #[derive(Debug)]
